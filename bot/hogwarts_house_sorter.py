a93701a5d5c0d057cee4012f8331e8d24d4b9ab4
nicoleepp <nickykepp@yahoo.ca>
2016-05-04 18:19:43 -0500

    House sorting fix again

--- bot/hogwarts_house_sorter.py
+++ bot/hogwarts_house_sorter.py
@@ -1,8 +1,9 @@
 import random
 
 HOUSES = ['Gryffindor','Ravenclaw','Slytherin','Hufflepuff']
+
 class HogwartsHouseSorter(object):
-	def get_random_house():
+	def get_random_house(self):
 		return random.choice(HOUSES)
 
 
