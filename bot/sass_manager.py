f0458989710f1ab5d4c6289e2a0f45aa9a26a9a9
Ian Gonzales <ianadmu@gmail.com>
2016-05-04 22:58:47 -0500

    removed self references /srhrug

--- bot/sass_manager.py
+++ bot/sass_manager.py
@@ -8,26 +8,26 @@ class SassManager(object):
         self.sass_cache = list()
         self.cache_loaded = False
 
-    def load_sass_cache(self):
+    def load_sass_cache():
         for line in self.sass_file.read().splitlines():
             self.sass_cache.append(line)
 
-    def get_sass(self, msg):
+    def get_sass(msg):
         return self.format_sass(self, msg)
 
-    def format_sass(self, msg):
+    def format_sass(msg):
         target = self.get_target(msg)
         sass = self.get_random_sass(self)
         return "Hey, " + target + "! " + sass
 
-    def get_random_sass(self):
+    def get_random_sass():
         if not self.cache_loaded:
             self.load_sass_cache()
             self.cache_loaded = True
 
     def get_target(msg):
         tokens = msg.split("sass ")
-        target = format_target(target.lower())
+        target = self.format_target(target.lower())
 
     def format_target(target):
         if target is "me":
