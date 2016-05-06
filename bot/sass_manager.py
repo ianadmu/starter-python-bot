d07d37c9d66c8a8be2430555fca85dd984b88d67
nicoleepp <nickykepp@yahoo.ca>
2016-05-06 16:14:05 -0500

    More fixes for sass tags

--- bot/sass_manager.py
+++ bot/sass_manager.py
@@ -10,25 +10,25 @@ class SassManager(object):
         self.sass_file = open(os.path.join('./resources', 'sass.txt'), 'r')
         self.sass_cache = self.sass_file.readlines()
 
-    def get_sass(self, user_id, msg):
+    def get_sass(self, msg):
         return self.format_sass(user_id, msg)
 
-    def format_sass(self, user_id, msg):
-        target = self.get_target(user_id, msg)
+    def format_sass(self, msg):
+        target = self.get_target(msg)
         sass = random.choice(self.sass_cache)
-        return 'Hey, <@{}>! {}'.format(user_id, sass)
+        return 'Hey, {}! {}'.format(target, sass)
 
-    def get_target(self, user_id, msg):
+    def get_target(self, msg):
         token = re.split(sass_flag, msg)
-        target = self.format_target(token[1].lower().encode('ascii','ignore'), user_id)
+        target = self.format_target(token[1].lower().encode('ascii','ignore'))
         return target
 
-    def format_target(self, target, user_id):
+    def format_target(self, target):
         if 'me' in target:
             return "you"
         elif 'yourself' in target:
             return "Zac Efron"
         elif target.startswith('@'):
-            return '<@{}>'.format(user_id)
+            return '<@{}>'.format(target)
         else:
             return target.title()
