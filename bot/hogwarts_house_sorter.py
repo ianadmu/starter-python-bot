65b51f940c68d07dbbc64c07dc6d5d66bc141eb5
nicoleepp <nickykepp@yahoo.ca>
2016-05-05 20:25:10 -0500

    Removed magic numbers from house sorter

--- bot/hogwarts_house_sorter.py
+++ bot/hogwarts_house_sorter.py
@@ -2,23 +2,24 @@ import random
 import xml.etree.ElementTree as ET   
 
 HOUSES = ['GRYFFINDOR','RAVENCLAW','SLYTHERIN','HUFFLEPUFF']
+NUM_DESCRIPTIONS = 6
 
 class HogwartsHouseSorter(object):
 
 	def get_house(self, msg):
 		msg = msg.upper();
-		index = int(random.random()*4)
-		for num in range(4):
+		index = int(random.random()*len(HOUSES))
+		for num in range(len(HOUSES)):
 			if HOUSES[index] in msg.upper():
 				return HOUSES[index]
 			else:
-				index = (index+1)%4
+				index = (index+1)%len(HOUSES)
 		return random.choice(HOUSES)
 
 	def get_house_description(self, house):
 		tree = ET.parse('house_descriptions.xml')
 		root = tree.getroot()
-		random_Number = int(random.random()*6) #min num of descriptions in the xml file per house
+		random_Number = int(random.random()*NUM_DESCRIPTIONS)
 		return root[HOUSES.index(house)][random_Number].text
 
 	def sort_into_house(self, msg):
