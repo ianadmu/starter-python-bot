d5e9252450244870ee3240fd1c2b51424ba47a34
jadxie <jade.hix@gmail.com>
2016-05-05 23:15:07 -0500

    silliness

--- bot/apology_manager.py
+++ bot/apology_manager.py
@@ -6,7 +6,7 @@ class ApologyManager(object):
     
     def __init__(self)
         self.apologies = open(os.path.join('./resources', 'apologies.txt'), 'r')
-        self.apology_cache = self.sass_file.readlines()
+        self.apology_cache = self.apologies.readlines()
     
     def get_random_apology(self):
         return random.choice(self.apology_cache)
