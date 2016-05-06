daafed46e571d4b40aa08b987bd7507fd176b561
jadxie <jade.hix@gmail.com>
2016-05-05 22:15:23 -0500

    changing calls to zac's responses

--- bot/event_handler.py
+++ bot/event_handler.py
@@ -51,15 +51,16 @@ class RtmEventHandler(object):
 
             if self.is_loud(msg_txt):
                 self.msg_writer.write_loud(event['channel'],msg_txt)
-
-            if self.clients.is_bot_mention(msg_txt):
+            
+            if re.search('thanks|thank you|thank-you', msg_txt) and self.clients.is_bot_mention(msg_txt):
+                self.msg_writer.write_your_welcome(event['channel'], event['user'])
+            
+            if msg_txt.startswith('zac '):
                 # e.g. user typed: "@pybot tell me a joke!"
                 if 'help' in msg_txt:
                     self.msg_writer.write_help_message(event['channel'])
                 elif re.search('hi|hey|hello|howdy|Hi|Hello|sup', msg_txt):
                     self.msg_writer.write_greeting(event['channel'], event['user'])
-                elif re.search('thanks|thank you|thank-you', msg_txt):
-                    self.msg_writer.write_your_welcome(event['channel'], event['user'])
                 elif 'joke' in msg_txt:
                     self.msg_writer.write_joke(event['channel'])
                 elif 'attachment' in msg_txt:
