1d2539af2e46119c4c1100ca2db4f52432675d50
nicoleepp <nickykepp@yahoo.ca>
2016-05-06 20:03:04 -0500

    test timed event

--- bot/messenger.py
+++ bot/messenger.py
@@ -132,8 +132,9 @@ class Messenger(object):
         self.send_message(channel_id, self.apology_manager.get_random_apology())
 
     def write_test(self, channel_id):
-        self.clients.send_user_typing_pause(channel_id)
-        self.send_message(channel_id, 'This is a test time triggered event')
+        self.api_call('im.history', channel='zacefron-dev')
+        #self.clients.send_user_typing_pause(channel_id)
+        #self.send_message(channel_id, 'This is a test time triggered event')
 
     def write_solution(self,channel_id, msg):
         self.send_message(channel_id, self.equation_manager.solve(msg))
