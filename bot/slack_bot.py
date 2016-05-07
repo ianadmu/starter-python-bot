b87d18490a1f7e1bd8faabbfc7f90542f8be18bc
nicoleepp <nickykepp@yahoo.ca>
2016-05-06 21:51:27 -0500

    last try for now

--- bot/slack_bot.py
+++ bot/slack_bot.py
@@ -77,7 +77,7 @@ class SlackBot(object):
         minute = int(time.strftime('%M'))
         second = int(time.strftime('%S'))
         if(second > 20 and second < 30):
-            self.clients.rtm.server.channels.find("#boardgames").send_message("this is a message")
+            self.clients.rtm.server.channels.find("<#boardgames>").send_message("this is a message")
 
     def stop(self, resource):
         """Stop any polling loops on clients, clean up any resources,
