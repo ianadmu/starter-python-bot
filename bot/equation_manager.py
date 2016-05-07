4ea0cd8d4a07ae630af1b74fdea133e4d7aed7e9
Malcolm Mergulhao <mmerg_1995@hotmail.com>
2016-05-06 19:39:50 -0500

    indentation

--- bot/equation_manager.py
+++ bot/equation_manager.py
@@ -5,10 +5,10 @@ class EquationManager(object):
 	def solve(self,equation):
 
 		tokens = equation.split()
-		 if(len(tokens)!= 3):
-		 	return "Ask me to solve an equation by saying 'zac solve <equation>'"
+		if(len(tokens)!= 3):
+			return "Ask me to solve an equation by saying 'zac solve <equation>'"
 
-		 try:
+		try:
 			return "The answer is "+str(eval(tokens[2]))+"!"
 		except:
 			return "I coulden't solve that equation :confounded:"
