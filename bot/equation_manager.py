edb2e310dfc69d10bd919342121d0f6346271b50
Malcolm Mergulhao <mmerg_1995@hotmail.com>
2016-05-06 19:08:50 -0500

    use len

--- bot/equation_manager.py
+++ bot/equation_manager.py
@@ -3,9 +3,9 @@ import re
 class EquationManager(object):
 	
 	def solve(self,equation):
-		
+
 		tokens = equation.split()
-		if(tokens.length != 3):
+		if(len(tokens)!= 3):
 			return "Ask me to solve an equation by saying 'zac solve <equation>'"
 
 		try:
