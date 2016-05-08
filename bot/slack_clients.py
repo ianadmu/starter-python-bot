9e36e62f49a86c812c393aa6ef8263353b98009d
nicoleepp <nickykepp@yahoo.ca>
2016-05-07 22:44:06 -0500

    getting somewhere

--- bot/slack_clients.py
+++ bot/slack_clients.py
@@ -55,5 +55,5 @@ class SlackClients(object):
     def get_random_emoji(self):
     	response = self.rtm.api_call('emoji.list')
     	emojis = response['emoji'].items()
-    	return emojis
-    	#return emojis[int(random.random()*58)]
+    	#return emojis
+    	return emojis[int(random.random()*58)]
