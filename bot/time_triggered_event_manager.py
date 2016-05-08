3d80fd3fd26f59410f6bb55dbaf6f7ec5a65ae8b
nicoleepp <nickykepp@yahoo.ca>
2016-05-08 01:18:06 -0500

    turned pings off and turned 945 on

--- bot/time_triggered_event_manager.py
+++ bot/time_triggered_event_manager.py
@@ -16,10 +16,10 @@ class TimeTriggeredEventManager(object):
         self.clients.send_time_triggered_msg('#zacefron-testing', msg)
 
     def trigger_945(self):
-        tag_users = ['nicole', 'here']
+        tag_users = ['channel', 'here']
         kip_msgs = ['@945', '945!', '#945', ':paw_prints: 945!', '~945~', ':horse: 945! giddyup', '945! :heart:', '945! :sweet_potato:', '945!........', '945 time', '945 quickie', '945 o\'clock', '945! :sheep: :panda_face: :slowpoke:', '945! :boom:', ':eggplant: 945.', '945 :coffee:', '_le 945_', '_le fast 945_']
         txt = '<@{}> {} :{}:'.format(random.choice(tag_users), random.choice(kip_msgs), self.clients.get_random_emoji())
-        self.clients.send_time_triggered_msg('#zacefron-testing', txt)
+        self.clients.send_time_triggered_msg('#random', txt)
 
     def trigger_timed_event(self):
         curr_datetime = datetime.utcnow() - timedelta(hours=HOUR_DIFFERENCE_DAYLIGHT_SAVINGS) #change here when daylight savings ends
@@ -28,7 +28,7 @@ class TimeTriggeredEventManager(object):
         minute = int(curr_datetime.strftime('%M'))
         second = int(curr_datetime.strftime('%S'))
         if(second >= 5 and second <= 15):
-            self.trigger_ping(day, hour, minute, second)
-            #if(hour == 9 and minute == 45):
-            self.trigger_945()
+            #self.trigger_ping(day, hour, minute, second)
+            if(hour == 9 and minute == 45):
+                self.trigger_945()
 
