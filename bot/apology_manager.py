781bdf0cbc4a0cc51c80ef9ff28724a4e19f7ed4
jadxie <jade.hix@gmail.com>
2016-05-09 12:07:08 -0500

    typo

--- bot/apology_manager.py
+++ bot/apology_manager.py
@@ -6,7 +6,7 @@ class ApologyManager(object):
 
     def __init__(self):
         self.apology_file = open(os.path.join('./resources', 'apologies.txt'), 'r')
-        self.apologies = self.apologies.readlines()
+        self.apologies = self.apology_file.readlines()
 
     def get_random_apology(self):
         return random.choice(self.apologies)
