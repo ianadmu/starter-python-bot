dc800e7ed8a675cec3b618ba95b63783b487aded
nicoleepp <nickykepp@yahoo.ca>
2016-05-09 18:13:04 -0500

    trying random events again

--- bot/time_triggered_event_manager.py
+++ bot/time_triggered_event_manager.py
@@ -9,10 +9,19 @@ class TimeTriggeredEventManager(object):
 
     def __init__(self, slack_clients):
         self.clients = slack_clients
+        self.last_random = 0
+        self.random_interval = 0
 
     def trigger_ping(self, day, hour, minute, second):
         random_custom_emoji = self.clients.get_random_emoji()
         msg = 'Ping on ' + day + ' ' + str(hour)  + ':' + str(minute) + ':' + str(second) + ' :' + str(random_custom_emoji) + ':' 
+        self.clients.send_time_triggered_msg('#zacefron-testing', msg)
+
+    def trigger_random(self, last_random, random_interval, new_random):
+        #channels = ['zacefron-testing', 'zacefron-testing']
+        kip_msgs = ['this is a random message', 'this is random message number 2', 'random message number 3']
+        msg = 'Last random minute: {} and last random interval: {} and new random interval: {} and Message: {}'.format(last_random, random_interval, new_random, random.choice(kips_msgs))
+        #channel = '#{}'.format(random.choice(channels)) 
         self.clients.send_time_triggered_msg('#zacefron-testing', msg)
 
     def trigger_945(self):
@@ -29,7 +38,12 @@ class TimeTriggeredEventManager(object):
         minute = int(curr_datetime.strftime('%M'))
         second = int(curr_datetime.strftime('%S'))
         if(second >= 5 and second <= 15):
-            #self.trigger_ping(day, hour, minute, second)
+            self.trigger_ping(day, hour, minute, second)
+            if minute >= ((self.last_random + self.random_interval)%60):
+                new_random = int(random.random()*5) + 1
+                self.trigger_random(self.last_random, self.random_interval, new_random)
+                self.random_interval = new_random
+                self.last_random = minute
             if hour == 9 and minute == 45:
                 self.trigger_945()
 
