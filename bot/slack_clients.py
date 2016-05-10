7c703de66510e78b10740ce012fb67b264a68208
nicoleepp <nickykepp@yahoo.ca>
2016-05-09 23:02:15 -0500

    tryyy

--- bot/slack_clients.py
+++ bot/slack_clients.py
@@ -50,4 +50,4 @@ class SlackClients(object):
 
     def upload_file_to_slack(self): 
     	my_file = open(os.path.join('./resources', 'pokemon_correct.txt')) 
-    	response = self.rtm.api_call('files.upload', filename='pokemon_correct.txt', file={'pokemon_correct.txt': open(os.path.join('./resources', 'pokemon_correct.txt'), 'rb')}, channels=['#zacefron-testing'])
+    	response = self.rtm.api_call('files.upload', filename='pokemon_correct.txt', file={'pokemon_correct.txt': open(os.path.join('./resources', 'pokemon_correct.txt'), 'rb')}, channels='#zacefron-testing')
