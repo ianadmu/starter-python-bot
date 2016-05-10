99bcbc6682512be58431722fbc463acf03fe456d
nicoleepp <nickykepp@yahoo.ca>
2016-05-09 23:28:48 -0500

    last try

--- bot/slack_clients.py
+++ bot/slack_clients.py
@@ -49,5 +49,7 @@ class SlackClients(object):
     	return emojis[int(random.random()*len(emojis))][0]
 
     def upload_file_to_slack(self): 
-    	#my_file = open(os.path.join('./resources', 'pokemon_correct.txt'))  #files = {'file': open('test.png', 'rb')}
-    	self.rtm.api_call('files.upload', filename='pokemon_correct.txt', file=open(os.path.join('./resources', 'pokemon_correct.txt'), 'rb'), channels='#zacefron-testing')
+    	#self.web.files.upload
+    	my_file = open(os.path.join('./resources', 'pokemon_correct.txt'))  #files = {'file': open('test.png', 'rb')}
+    	self.web.files.upload(my_file)
+    	#self.rtm.api_call('files.upload', filename='pokemon_correct.txt', file=open(os.path.join('./resources', 'pokemon_correct.txt'), 'rb'), channels='#zacefron-testing')
