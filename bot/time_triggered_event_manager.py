683437d4806da4ac93b3dd1cb87f4cfbc9fe1046
Kiera <keenns12@hotmail.com>
2016-05-10 23:08:04 -0500

    why you no work zac

--- bot/time_triggered_event_manager.py
+++ bot/time_triggered_event_manager.py
@@ -104,7 +104,7 @@ class TimeTriggeredEventManager(object):
             self.check_trigger_random(day, hour, minute, second) #in own method because math
             if hour == 9 and minute == 45:
                 self.trigger_945()
-            if hour == 23:
+            elif hour == 23:
                 self.trigger_mochaccino()
             if day == 'Friday':
                 if hour == 16 and minute == 30:
