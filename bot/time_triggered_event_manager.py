ef67d138c330bd719a6dba8811765523418f289a
nicoleepp <nickykepp@yahoo.ca>
2016-05-10 21:31:29 -0500

    set up startup time log to testing channel

--- bot/time_triggered_event_manager.py
+++ bot/time_triggered_event_manager.py
@@ -17,10 +17,16 @@ class TimeTriggeredEventManager(object):
         self.last_random_minutes = 0
         self.random_interval_minutes = 0
         self.random_hasnt_fired = True
+        self.is_just_starting_up = True
 
     def trigger_ping(self, day, hour, minute, second):
         random_custom_emoji = self.clients.get_random_emoji()
         msg = 'Ping on ' + day + ' ' + str(hour)  + ':' + str(minute) + ':' + str(second) + ' :' + str(random_custom_emoji) + ':' 
+        self.clients.send_time_triggered_msg('#zacefron-testing', msg)
+
+    def trigger_startup_log(self, day, hour, minute, second):
+        random_custom_emoji = self.clients.get_random_emoji()
+        msg = 'I came back to life on ' + day + ' ' + str(hour)  + ':' + str(minute) + ':' + str(second) + ' :' + str(random_custom_emoji) + ':' 
         self.clients.send_time_triggered_msg('#zacefron-testing', msg)
 
     def trigger_random(self, new_random_minutes):
@@ -35,8 +41,9 @@ class TimeTriggeredEventManager(object):
     def check_trigger_random(self, day, hour, minute, second):
         random_should_fire_hr = (self.last_random_hour + int(self.random_interval_minutes/MIN_PER_HOUR) + int((self.last_random_minutes + self.random_interval_minutes%MIN_PER_HOUR)/MIN_PER_HOUR))%HR_PER_DAY
         random_should_fire_min = (self.last_random_minutes + self.random_interval_minutes%MIN_PER_HOUR)%MIN_PER_HOUR #math
-        if self.random_hasnt_fired or (hour == random_should_fire_hr and minute == random_should_fire_min): 
-            new_random_minutes = int(random.random()*174) + 1 #fire at least every 5 minutes
+        if self.random_hasnt_fired or (hour == random_should_fire_hr and minute == random_should_fire_min):
+            max_minutes_between_random_events = 174
+            new_random_minutes = int(random.random()*max_minutes_between_random_events) + 1
             #if hour > 8 and hour < 22: #ping and random event fire for testing #when done testing uncomment and indent the next 2 lines when you uncomment!!!!
             self.trigger_ping(day, hour, minute, second)
             self.trigger_random(new_random_minutes)
@@ -55,12 +62,20 @@ class TimeTriggeredEventManager(object):
         self.clients.send_time_triggered_msg('#random', txt)
 
     def trigger_timed_event(self):
+        #get date and time
         curr_datetime = datetime.utcnow() - timedelta(hours=HOUR_DIFFERENCE_DAYLIGHT_SAVINGS) #change here when daylight savings ends
         day = curr_datetime.strftime('%A')
         hour = int(curr_datetime.strftime('%H'))
         minute = int(curr_datetime.strftime('%M'))
         second = int(curr_datetime.strftime('%S'))
-        if(second >= 5 and second <= 15): #leave a bit over 10 seconds to trigger, method is called every 10-ish s and we only want it to trigger once per min
+
+        #trigger startup log to testing channel
+        if(self.is_just_starting_up):
+            self.trigger_startup_log(day, hour, minute, second)
+            self.is_just_starting_up = False
+
+        #trigger timed or random events leave a bit over 10 seconds to trigger, method is called every 10-ish s and we only want it to trigger once per min
+        if(second >= 5 and second <= 15):
             #self.trigger_ping(day, hour, minute, second)
             self.check_trigger_random(day, hour, minute, second) #in own method because math
             if hour == 9 and minute == 45:
