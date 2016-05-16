749331f599190e80653c5ecf520afb119ba2a1eb
jadxie <jade.hix@gmail.com>
2016-05-16 10:25:09 -0500

    pokemon name is first

--- bot/pokemon_caster.py
+++ bot/pokemon_caster.py
@@ -2,7 +2,6 @@ import json
 import requests
 import re
 
-pkmn_flag = re.compile('choose you ')
 URL =  'http://pokeapi.co/api/v2/pokemon/{}/'
 
 class PokemonCaster(object):
@@ -10,7 +9,7 @@ class PokemonCaster(object):
     def i_choose_you(self, msg):
         token = re.split(pkmn_flag, msg)
         link = URL
-        target = link.format(token[1])
+        target = link.format(token[0])
         try:
             response = requests.get(target)
         except requests.exceptions.RequestException as e:
