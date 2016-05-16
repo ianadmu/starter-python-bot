e8bac3f06b3369d92bc9ea55daef6450a0e97393
Haruka <Haruka>
2016-05-15 19:45:05 -0500

    reverted change

--- bot/pokemon_caster.py
+++ bot/pokemon_caster.py
@@ -2,7 +2,7 @@ import json
 import requests
 import re
 
-pkmn_flag = re.compile(' (I|i) choose you')
+pkmn_flag = re.compile('(I|i) choose you ')
 URL =  'http://pokeapi.co/api/v2/pokemon/{}/'
 
 class PokemonCaster(object):
@@ -10,7 +10,7 @@ class PokemonCaster(object):
     def i_choose_you(self, msg):
         token = re.split(pkmn_flag, msg)
         link = URL
-        target = link.format(token[0])
+        target = link.format(token[1])
         try:
             response = requests.get(target)
         except requests.exceptions.RequestException as e:
