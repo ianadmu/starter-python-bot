f6d24bb92dd74c65b2db432b6d9312d80299518f
Haruka <Haruka>
2016-05-15 19:28:48 -0500

    hopefully debugged

--- bot/pokemon_caster.py
+++ bot/pokemon_caster.py
@@ -6,14 +6,14 @@ URL =  'http://pokeapi.co/api/v2/pokemon/{}/'
 
 class PokemonCaster(object):
                  
-    def i_choose_you(self, pkmn):
+    def i_choose_you(self, msg):
         token = re.split('choose you ', msg)
         link = URL
         target = link.format(token[1])
         try:
             response = requests.get(target)
         except requests.exceptions.RequestException as e:
-            return 'nope, something went wrong there pal'
+            return None
         else:
             pokemon = response.json()
             return pokemon['sprites']['front_default']
