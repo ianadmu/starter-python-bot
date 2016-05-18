62094746f6d86bf9099780fcbc011d7439fa8d36
nicoleepp <nickykepp@yahoo.ca>
2016-05-17 21:03:24 -0500

    testing again

--- bot/time_triggered_event_manager.py
+++ bot/time_triggered_event_manager.py
@@ -55,7 +55,7 @@ class TimeTriggeredEventManager(object):
             self.random_interval_minutes = new_random_minutes
             if self.random_hasnt_fired:
                 self.clients.upload_file_to_slack() #test file upload
-                self.list_files_on_slack()
+                self.clients.list_files_on_slack()
                 self.random_hasnt_fired = False
 
     def trigger_wine_club(self):
