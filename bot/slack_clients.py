63beded31e16409d3587d12f3b958419c0b29abe
nicoleepp <nickykepp@yahoo.ca>
2016-05-17 22:42:40 -0500

    forgot to change somehting

--- bot/slack_clients.py
+++ bot/slack_clients.py
@@ -59,4 +59,4 @@ class SlackClients(object):
     	response = self.rtm.api_call('files.info', file='F19NX4WJD')
     	#response = self.web.files.list()
     	#files = response['files'].items()
-    	self.send_time_triggered_msg('#zacefron-testing', response)
+    	self.rtm.api_call('chat.postMessage', as_user='true:', channel='#zacefron-testing', text=response)
