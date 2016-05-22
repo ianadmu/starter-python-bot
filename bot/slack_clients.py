c006b9ebef80cdb0c93177fa549b9a6e9f86b12e
nicoleepp <nickykepp@yahoo.ca>
2016-05-22 03:47:24 -0500

    more testting

--- bot/slack_clients.py
+++ bot/slack_clients.py
@@ -67,7 +67,7 @@ class SlackClients(object):
     def get_file_info(self): 
     	response = self.rtm.api_call('files.info', file='F19NX4WJD')
     	files = response['file'].items()
-    	self.rtm.api_call('chat.postMessage', as_user='true:', channel='#zacefron-testing', text=files[12][0])
+    	self.rtm.api_call('chat.postMessage', as_user='true:', channel='#zacefron-testing', text=files[13][0])
     	response2 = self.rtm.api_call('users.info', user='U15FDSK5M')
     	user_info = response2['user'].items()
-    	self.rtm.api_call('chat.postMessage', as_user='true:', channel='#zacefron-testing', text=user_info[1])
+    	self.rtm.api_call('chat.postMessage', as_user='true:', channel='#zacefron-testing', text=user_info[0])
