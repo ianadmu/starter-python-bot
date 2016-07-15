17002310d1bef7479b312c3bfb5801b282a1330c
Malcolm Mergulhao <mmerg_1995@hotmail.com>
2016-07-14 20:52:39 -0500

    now 945 and wine club will work in hash land

--- bot/time_triggered_event_manager.py
+++ bot/time_triggered_event_manager.py
@@ -64,7 +64,7 @@ class TimeTriggeredEventManager(object):
         tag_users = ['channel', 'here']
         msg = 'WINE CLUB IN THE LOUNGE :wine_glass: :wine_glass: :wine_glass: :wine_glass: :wine_glass:'
         txt = '<!{}> {}'.format(random.choice(tag_users), msg) 
-        self.clients.send_time_triggered_msg('#heliwolves-announce', txt)
+        self.clients.send_time_triggered_msg('#general', txt)
 
     def trigger_drunk_phrase(self):
         channels = ['heliwolves', 'spamalot', 'random']
@@ -84,7 +84,7 @@ class TimeTriggeredEventManager(object):
         tag_users = ['channel', 'here']
         kip_msgs = ['@945', '945!', '#945', ':paw_prints: 945!', '~945~', ':horse: 945! giddyup', '945! :heart:', '945! :sweet_potato:', '945!........', '945 time', '945 quickie', '945 o\'clock', '945! :sheep: :panda_face: :slowpoke:', '945! :boom:', ':eggplant: 945.', '945 :coffee:', '_le 945_', '_le fast 945_']
         txt = '<!{}> {} :{}:'.format(random.choice(tag_users), random.choice(kip_msgs), random_custom_emoji) 
-        self.clients.send_time_triggered_msg('#heliwolves-announce', txt)
+        self.clients.send_time_triggered_msg('#general', txt)
         digg_options = ['edition', 'tech', 'technology', 'computer', 'computers', 'fun', 'neowin', '', 'trending', 'programmer']
         digg_msg = '/digg {}'.format(random.choice(digg_options))
         self.clients.send_time_triggered_msg('#zacefron-testing', digg_msg)
