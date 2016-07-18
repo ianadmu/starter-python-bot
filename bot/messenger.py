bc1a8e988e68538b302001998c64c8d6ea2fee6b
malcolm-mergulhao <mmerg_1995@hotmail.com>
2016-07-17 19:16:01 -0500

    last try for unicode

--- bot/messenger.py
+++ bot/messenger.py
@@ -35,9 +35,9 @@ class Messenger(object):
         # in the case of Group and Private channels, RTM channel payload is a complex dictionary
         if isinstance(channel_id, dict):
             channel_id = channel_id['id']
-        logger.debug('Sending msg: {} to channel: {}'.format(msg, channel_id))
+        #logger.debug('Sending msg: {} to channel: {}'.format(msg, channel_id))
         channel = self.clients.rtm.server.channels.find(channel_id)
-        channel.send_message("{}".format(msg))
+        channel.send_message(msg)
 
     def write_message_deleted(self, channel_id):
         txt = 'I SAW THAT'
