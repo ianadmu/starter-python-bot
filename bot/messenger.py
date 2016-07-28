a2d03905e2349d3b272b7a119e66ce7ab88aa5d4
Garrett Suss <garrettsuss@gmail.com>
2016-07-27 21:43:32 -0500

    re added time delay

--- bot/messenger.py
+++ bot/messenger.py
@@ -5,6 +5,7 @@ import random
 import re
 import os.path
 import threading
+import time
 from datetime import datetime
 from loud_manager import LoudManager
 from whos_that_pokemon_manager import WhosThatPokemonManager
@@ -37,6 +38,7 @@ class Messenger(object):
 
     def send_message(self, channel_id, msg):
         # in the case of Group and Private channels, RTM channel payload is a complex dictionary
+        time.sleep(0.5)
         if isinstance(channel_id, dict):
             channel_id = channel_id['id']
         #logger.debug('Sending msg: {} to channel: {}'.format(msg, channel_id))
