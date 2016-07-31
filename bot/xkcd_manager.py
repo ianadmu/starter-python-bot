4812f2164713624fe753381e8a4d45c613a846a2
Garrett Suss <garrettsuss@gmail.com>
2016-07-31 10:45:08 -0500

    Added title fetching for xkcd_manager
    stop trying to make fetch a thing

--- bot/xkcd_manager.py
+++ bot/xkcd_manager.py
@@ -21,21 +21,30 @@ def parseComicRequest(comicRequest):
 
 def getImageLocation(comicRequest):
 
+	titleString = 'id="ctitle">'
+	captionString = 'title="'
+	imageString = '//imgs.xkcd.com/comics/'
+
 	response = urllib2.urlopen(parseComicRequest(comicRequest))
 	html = response.read()
 
-	imageAddressStart = html.find('//imgs.xkcd.com/comics/')
+	titleStart = html.find(titleString) + len(titleString)
+	titleEnd = html[titleStart:].find('<') + titleStart
+	title = html[titleStart:titleEnd]
+
+	imageAddressStart = html.find(imageString)
 	imageAddressEnd = html[imageAddressStart:].find('"') + imageAddressStart
 	imageAddress = html[imageAddressStart:imageAddressEnd]
 
-	captionStart = html[imageAddressEnd:].find('title="') + imageAddressEnd + 7
+	captionStart = html[imageAddressEnd:].find(captionString) + imageAddressEnd + len(captionString)
 	captionEnd = html[captionStart:].find('"') + captionStart
 	caption = html[captionStart:captionEnd]
 
 	parser = HTMLParser()
 	caption = parser.unescape(caption)
+	title = parser.unescape(title)
 
-	return "http:" + str(imageAddress) + '\n' + caption
+	return '*' + title + "*\nhttp:" + str(imageAddress) + '\n' + caption
 
 def getCurrentMaxComic():
 	response = urllib2.urlopen("http://xkcd.org/")
