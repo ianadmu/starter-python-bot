4c6844e66e3146057574408c04da8c30e949ab8f
Garrett Suss <garrettsuss@gmail.com>
2016-07-31 23:02:58 -0500

    Forgot another self

--- bot/tictactoe_manager.py
+++ bot/tictactoe_manager.py
@@ -24,7 +24,7 @@ class TicTacToeManager:
 	def start(self, channel, size, length):
 		self.msg_writer.send_message(channel,str(size) + " " + str(length))
 		self.games[channel] = TicTacToe(size, length)
-		self.msg_writer.send_message(channel,games[channel].__str__())
+		self.msg_writer.send_message(channel,self.games[channel].__str__())
 
 	def process_command(self, channel, command):
 		result = games[channel].process_command(command)
