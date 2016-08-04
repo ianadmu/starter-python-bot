d69beb537da1931aca35fa3a1ac08c6156488715
Garrett Suss <garrettsuss@gmail.com>
2016-08-04 00:55:27 -0500

    debug

--- bot/response_master.py
+++ bot/response_master.py
@@ -31,7 +31,7 @@ class Response:
 		return self.responses[hashValue % len(self.responses)]
 
 	def random(self):
-		return str(self.responses) + " " + self.triggers
+		return str(self.responses) + " " + str(self.triggers)
 		# return random.choice(self.responses)
 
 class Response_master:
