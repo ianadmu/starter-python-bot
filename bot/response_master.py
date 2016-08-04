d568c8df560330f301edbe9d2c358b34c428ee95
Garrett Suss <garrettsuss@gmail.com>
2016-08-04 01:10:29 -0500

    does it work like this?

--- bot/response_master.py
+++ bot/response_master.py
@@ -40,7 +40,7 @@ class Response_master:
 		json_events = json.load(master_file)
 		self.events = []
 		for event in json_events["Events"]:
-			use_hash = "Hash" not in event or event["Hash"]
+			use_hash = event["Hash"]
 			triggers = []
 			responses = []
 			for t in event["Triggers"]:
