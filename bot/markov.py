427175f24989745615f9ed633a9760052e960445
Garrett Suss <garrettsuss@gmail.com>
2016-09-18 12:26:22 -0400

    reduced markov chain maximum length to 100 words

--- bot/markov.py
+++ bot/markov.py
@@ -6,7 +6,7 @@ import random
 from collections import defaultdict
 from collections import deque
 class Markov:
-	maximum_length = 1000
+	maximum_length = 100
 	words_that_end_in_periods = ("Mrs", "Mr", "Co", "M", "Dr", "Ms", "Sr", "St", "Rd", "Ave", "Mt", "Blvd", "Org", "Ltd", "...")
 	terminators = [".", "!", "?"]
 	characters_to_remove = re.compile("[\n\"`\”—‘’\(\);]")
