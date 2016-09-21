f3c7c8a63ebb7e6ca87e40ca68edcd6bac758716
Nicole Epp <nicole.epp@invenia.ca>
2016-09-21 09:48:12 -0500

    quick fix so zac doesn't die in iq changels

--- bot/time_triggered_event_manager.py
+++ bot/time_triggered_event_manager.py
@@ -35,7 +35,10 @@ class TimeTriggeredEventManager(object):
 
     def trigger_method_log(self, method_name):
         msg = 'Event: {}'.format(method_name)
-        self.msg_writer.send_message('C1SDALDG9', msg)
+        try:
+            self.msg_writer.send_message(common.channels['slackers']['zac-testing'], msg)
+        except:
+            self.msg_writer.send_message(common.channels['iq']['zac-testing'], msg)
 
     def trigger_startup_log(self, day, hour, minute, second):
         random_custom_emoji = self.clients.get_random_emoji()
@@ -46,11 +49,13 @@ class TimeTriggeredEventManager(object):
             self.msg_writer.send_message(common.channels['iq']['zac-testing'], msg)
 
     def trigger_random(self):
-        channel = 'C15S4357W'
         tree = ET.parse(os.path.join('./resources', 'random_comments.xml'))
         root = tree.getroot()
         txt = root[int(random.random()*len(root))].text
-        self.msg_writer.send_message(channel, txt)
+        try:
+            self.msg_writer.send_message('C15S4357W', txt)
+        except:
+            self.msg_writer.send_message('C15S5LNM9', txt)
         self.trigger_method_log('random')
 
     def check_trigger_random(self, hour, minute):
@@ -86,17 +91,20 @@ class TimeTriggeredEventManager(object):
     def trigger_weather(self):
         response = weather_manager.getCurrentWeather()
         # response = WeatherController.get_weather()
-        self.msg_writer.send_message('C1SDALDG9', response)
+        try:
+            self.msg_writer.send_message(common.channels['slackers']['zac-testing'], response)
+        except:
+            self.msg_writer.send_message(common.channels['iq']['zac-testing'], response)
 
     def trigger_945(self):
         random_custom_emoji = self.clients.get_random_emoji()
         tag_users = ['channel', 'here']
         kip_msgs = ['@945', '945!', '#945', ':paw_prints: 945!', '~945~', ':horse: 945! giddyup', '945! :heart:', '945! :sweet_potato:', '945!........', '945 time', '945 quickie', '945 o\'clock', '945! :sheep: :panda_face: :slowpoke:', '945! :boom:', ':eggplant: 945.', '945 :coffee:', '_le 945_', '_le fast 945_']
         txt = '<!{}> {} :{}:'.format(random.choice(tag_users), random.choice(kip_msgs), random_custom_emoji)
-        self.msg_writer.send_message('C15S4357W', txt)
-        digg_options = ['edition', 'tech', 'technology', 'computer', 'computers', 'fun', 'neowin', '', 'trending', 'programmer']
-        digg_msg = '/digg {}'.format(random.choice(digg_options))
-        self.msg_writer.send_message('C1SDALDG9', digg_msg)
+        try:
+            self.msg_writer.send_message('C15S4357W', txt)
+        except:
+            self.msg_writer.send_message('C15S5LNM9', txt)
 
     def trigger_mochaccino(self):
         tag_users = ['channel', 'here']
@@ -106,7 +114,10 @@ class TimeTriggeredEventManager(object):
         'Today\'s mochaccino is like an angel pooped out a nice hot cup of coffee mmmmm~', 'Mochaccino status: passable',
         'MOCHACCINO MOCHACCINO MOCHACCINO!!!', 'Who\'s ready for a nice cup o\' mochaccino?', '_le mochaccino_']
         txt = '<!{}> {} :coffee:'.format(random.choice(tag_users), random.choice(msgs))
-        self.msg_writer.send_message('C15S4357W', txt)
+        try:
+            self.msg_writer.send_message('C15S4357W', txt)
+        except:
+            self.msg_writer.send_message('C15S5LNM9', txt)
 
     def trigger_timed_event(self):
         #get date and time
@@ -129,13 +140,12 @@ class TimeTriggeredEventManager(object):
                 self.trigger_weather()
             if minute == 15:
                 self.trigger_markov()
-            if day != 'Saturday' and day !='Sunday' and hour == 9 and minute == 45:
+            if day != 'Saturday' and day != 'Sunday' and hour == 9 and minute == 45:
                 self.trigger_945()
-            if day != 'Saturday' and day !='Sunday' and hour == 9 and minute == 0:
+            if day != 'Saturday' and day != 'Sunday' and hour == 9 and minute == 0:
                 self.trigger_mochaccino()
             if day == 'Friday':
                 if hour == 16 and minute == 30:
                     self.trigger_wine_club()
                 if (hour == 16 and minute == 35) or (hour == 17 and minute == 0) or (hour == 17 and minute == 30) or (hour == 18 and minute == 0):
                     self.trigger_drunk_phrase()
-
