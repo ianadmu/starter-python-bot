b4f3d8c61d606a402660bffe57d09d64e14b58c3
Nicole Epp <nicole.epp@invenia.ca>
2016-09-21 22:29:31 -0500

    debugging - why tabs why

--- bot/channel_manager.py
+++ bot/channel_manager.py
@@ -1,30 +1,31 @@
 import json
 
+
 class ChannelManager:
 
-	def __init__(self, clients):
-		channels = clients.get_channels()
-		self.channel_names = dict()
-		self.channel_ids = dict()
-		if self.channels["ok"]:
-			for channel in channels["channels"]:
-				self.channel_names[channel["id"]] = channel["name"]
-				self.channel_ids[channel["name"]] = channel["id"]
+    def __init__(self, clients):
+        channels = clients.get_channels()
+        self.channel_names = dict()
+        self.channel_ids = dict()
+        if self.channels["ok"]:
+            for channel in channels["channels"]:
+                self.channel_names[channel["id"]] = channel["name"]
+                self.channel_ids[channel["name"]] = channel["id"]
 
-	def get_channel_id(self, identifier):
-		if identifier in self.channel_names:
-			return self.channel_names[identifier]
-		elif identifier in self.channel_ids:
-			return identifier
-		else:
-			return None
+    def get_channel_id(self, identifier):
+        if identifier in self.channel_names:
+            return self.channel_names[identifier]
+        elif identifier in self.channel_ids:
+            return identifier
+        else:
+            return None
 
-	def get_channel_by_id(self, user_id):
-		if channel_id in self.channel_names:
-			return self.channel_names[channel_id]
-		return None
+    def get_channel_by_id(self, channel_id):
+        if channel_id in self.channel_names:
+            return self.channel_names[channel_id]
+        return None
 
-	def get_channel_by_name(self, name):
-		if name in self.channel_ids:
-			return self.channel_ids[name]
-		return None
+    def get_channel_by_name(self, name):
+        if name in self.channel_ids:
+            return self.channel_ids[name]
+        return None
