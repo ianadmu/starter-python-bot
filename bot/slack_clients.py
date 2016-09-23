8211663f9c107aaa18c5742c57395ebeaf838c55
Garrett Suss <garrettsuss@gmail.com>
2016-09-22 22:34:53 -0400

    as_user = true

--- bot/slack_clients.py
+++ bot/slack_clients.py
@@ -50,7 +50,8 @@ class SlackClients(object):
         return emojis[int(random.random()*len(emojis))][0]
 
     def send_message(self, channel_id, msg):
-        self.rtm.api_call("chat.postMessage", token=str(self.token), channel=channel_id, text=msg)
+        self.rtm.api_call("chat.postMessage", token=str(self.token),
+        channel=channel_id, text=msg, as_user=True)
 
     def get_users(self):
         return self.rtm.api_call("users.list", token=str(self.token))
