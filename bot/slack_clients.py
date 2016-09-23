7cfdea71f6f358d51ef9b826101523961c58e7ca
Garrett Suss <garrettsuss@gmail.com>
2016-09-23 00:11:33 -0400

    changing link names from true to 1

--- bot/slack_clients.py
+++ bot/slack_clients.py
@@ -51,12 +51,12 @@ class SlackClients(object):
 
     def send_message_as_other(self, channel_id, msg, name, emoji):
         self.rtm.api_call("chat.postMessage", token=str(self.token),
-        channel=channel_id, text=msg, link_names=True, username=name,
+        channel=channel_id, text=msg, link_names=1, username=name,
         unfurl_links=True, icon_emoji=emoji)
 
     def send_message(self, channel_id, msg):
         self.rtm.api_call("chat.postMessage", token=str(self.token),
-        channel=channel_id, text=msg, as_user=True, link_names=True,
+        channel=channel_id, text=msg, as_user=True, link_names=1,
         unfurl_links=True)
 
     def get_users(self):
