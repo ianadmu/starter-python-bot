d3334307af33d87f575828b66a150778264641cc
Nicole Epp <nicole.epp@invenia.ca>
2016-09-24 15:01:38 -0500

    standardizing

--- bot/user_manager.py
+++ bot/user_manager.py
@@ -1,38 +1,34 @@
-import json
-
 class UserManager:
-	
-	def __init__(self, clients, msg_writer):
-		self.users = clients.get_users()
-		self.user_names = dict()
-		self.user_ids = dict()
-		if self.users["ok"]:
-			for user in self.users["members"]:
-				self.user_names[user["id"]] = user["name"]
-				self.user_ids[user["name"]] = user["id"]
-		else:
-			if "error" in self.users:
-				msg_writer.write_custom_error(self.users["error"])
 
-	def print_all_users(self, msg_writer):
-		for name in self.user_ids:
-			msg_writer.write_custom_error(name + ": " + self.user_ids[name])
+    def __init__(self, clients, msg_writer):
+        self.users = clients.get_users()
+        self.user_names = dict()
+        self.user_ids = dict()
+        if self.users["ok"]:
+            for user in self.users["members"]:
+                self.user_names[user["id"]] = user["name"]
+                self.user_ids[user["name"]] = user["id"]
+        else:
+            if "error" in self.users:
+                msg_writer.write_custom_error(self.users["error"])
 
-	def get_user_by_id(self, user_id):
-		if user_id in self.user_names:
-			return self.user_names[user_id]
-		return None
+    def print_all_users(self, msg_writer):
+        for name in self.user_ids:
+            msg_writer.write_custom_error(name + ": " + self.user_ids[name])
 
-	def get_user_by_name(self, name):
-		if name in self.user_names:
-			return self.user_names[name]
-		return None
+    def get_user_by_id(self, user_id):
+        if user_id in self.user_names:
+            return self.user_names[user_id]
+        return None
 
-	def get_users_mentioned(self, message):
-		mentioned_users = set()
-		for token in message.split():
-			if token in self.user_ids:
-				mentioned_users.add(token)
-		return mentioned_users
+    def get_user_by_name(self, name):
+        if name in self.user_names:
+            return self.user_names[name]
+        return None
 
-		
+    def get_users_mentioned(self, message):
+        mentioned_users = set()
+        for token in message.split():
+            if token in self.user_ids:
+                mentioned_users.add(token)
+        return mentioned_users
--- bot/weather_manager.py
+++ bot/weather_manager.py
@@ -2,10 +2,17 @@
 # -*- coding: utf-8 -*-
 
 import urllib2
-import random
+
+from datetime import datetime, timedelta
 from HTMLParser import HTMLParser
 import re
 import ssl
+
+SUNSET_HOUR = 21
+SUNRISE_HOUR = 5
+HOUR_DIFFERENCE_DAYLIGHT_SAVINGS = 5  # for Winnipeg
+HOUR_DIFFERENCE_NO_DAYLIGHT_SAVINGS = 6  # for Winnipeg
+
 
 def get_icon(conds):
         if "tornado" in conds or "hurricane" in conds or "cyclone" in conds:
@@ -25,7 +32,11 @@ def get_icon(conds):
         if "sunny" in conds:
             return ":sunny:"
         if "clear" in conds:
-            curr_datetime = datetime.utcnow() - timedelta(hours=HOUR_DIFFERENCE_DAYLIGHT_SAVINGS) #change here when daylight savings ends
+            curr_datetime = (
+                datetime.utcnow() - timedelta(
+                    hours=HOUR_DIFFERENCE_DAYLIGHT_SAVINGS
+                )  # change here when daylight savings ends
+            )
             curr_time = int(curr_datetime.strftime('%H'))
             if curr_time >= SUNSET_HOUR or curr_time < SUNRISE_HOUR:
                 return ":night_with_stars:"
@@ -46,28 +57,34 @@ def get_icon(conds):
         else:
             return ":zacefron:"
 
+
 def scrapeItem(html, startString, endString, parser):
-	start = html.find(startString) + len(startString)
-	end = html[start:].find(endString) + start
-	return parser.unescape(html[start:end])
+    start = html.find(startString) + len(startString)
+    end = html[start:].find(endString) + start
+    return parser.unescape(html[start:end])
+
 
 def getCurrentWeather():
-	ssl._create_default_https_context = ssl._create_unverified_context
-	stringsBeforeGT = re.compile("(\n|.)*>")
-	url = "https://weather.gc.ca/city/pages/mb-38_metric_e.html"
-	conditionString = 'Condition:'
-	temperatureString = 'Temperature:'
-	temperatureEnd = '&deg;'
-	end = '</dd>'
-	parser = HTMLParser()
+    ssl._create_default_https_context = ssl._create_unverified_context
+    stringsBeforeGT = re.compile("(\n|.)*>")
+    url = "https://weather.gc.ca/city/pages/mb-38_metric_e.html"
+    conditionString = 'Condition:'
+    temperatureString = 'Temperature:'
+    temperatureEnd = '&deg;'
+    end = '</dd>'
+    parser = HTMLParser()
 
-	response = urllib2.urlopen(url)
-	html = response.read()
+    response = urllib2.urlopen(url)
+    html = response.read()
 
-	condition = scrapeItem(html, conditionString, end, parser)
-	temperature = scrapeItem(html, temperatureString, temperatureEnd, parser)
-	#these need additional scraping
-	condition = re.sub(stringsBeforeGT, "", condition)
-	temperature = re.sub(stringsBeforeGT, "", temperature)
+    condition = scrapeItem(html, conditionString, end, parser)
+    temperature = scrapeItem(html, temperatureString, temperatureEnd, parser)
+    # these need additional scraping
+    condition = re.sub(stringsBeforeGT, "", condition)
+    temperature = re.sub(stringsBeforeGT, "", temperature)
 
-	return "It is currently " + temperature + "°C and " + condition + "\n" + get_icon(condition)
+    result = (
+        "It is currently " + temperature + "°C and " + condition +
+        "\n" + get_icon(condition)
+    )
+    return result
