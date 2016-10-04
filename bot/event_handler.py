264bc8342b11cd44b2a742da6284bccd3e47042d
Nicole Epp <nicole.epp@invenia.ca>
2016-10-04 18:57:09 -0500

    debug

--- bot/event_handler.py
+++ bot/event_handler.py
@@ -54,7 +54,6 @@ class RtmEventHandler(object):
             self.msg_writer.write_help_message(event['channel'])
         elif event_type == 'message':
             # message was sent to channel
-            self.msg_writer.send_message('zac-testing', str(event))
             self._handle_message(event)
         elif event_type == 'group_joined':
             # you joined a private group
