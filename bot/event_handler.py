ef8985b7e342b5e6e605d0e60cdfbbc5874a58ef
Nicole Epp <nicole.epp@invenia.ca>
2016-10-11 20:53:03 -0500

    fix zac dying

--- bot/event_handler.py
+++ bot/event_handler.py
@@ -116,7 +116,7 @@ class RtmEventHandler(object):
             ts = event['ts']
             if event['type'] == 'reaction_added' and 'item' in event:
                 ts = event['item']['ts']
-                self.send_message(str(ts))
+                self.msg_writer.send_message(str(ts))
 
             user_name = self.user_manager.get_user_by_id(user_id)
             lower_txt = msg_txt.lower()
