6156b93eb76f2e7fb95cb1814c2edcd01b6146aa
malcolm-mergulhao <mmerg_1995@hotmail.com>
2016-10-22 15:24:36 -0500

    debug output

--- bot/terminal_manager.py
+++ bot/terminal_manager.py
@@ -1,8 +1,11 @@
+import logging
 import subprocess
 import time
 from threading import Timer
 
 MAX_RUN_TIME = 5
+
+logger = logging.getLogger(__name__)
 
 def run_terminal_command(command):
 	control_vars = {
@@ -14,6 +17,8 @@ def run_terminal_command(command):
 	}
 
 	def _run_command_wrapper():
+		logger.debug("Starting wrapper")
+		logger.debug(control_vars["command_tokens"])
 		process = subprocess.Popen(control_vars["command_tokens"], stdout=subprocess.PIPE)
 		control_vars["process"] = process
 		control_vars["dead_man_swicth_timer"].start()
@@ -23,9 +28,10 @@ def run_terminal_command(command):
 		if process_killed:
 			return "I took to long to run a command!"
 	 	elif error:
+	 		logger.debug("Error: "+error)
 	 		return "Something went wrong while executing that command: "+error
 	 	else:
-
+	 		logger.debug("output: "+error)
 	 		return "> "+output
 
 
@@ -34,5 +40,6 @@ def run_terminal_command(command):
 			if control_vars["process"]:
 				control_vars["process"].kill()	
 
-	control_vars["dead_man_swicth_timer"] = Timer(MAX_RUN_TIME, terminal_manager._dead_man_swicth).start()
+	control_vars["dead_man_swicth_timer"] = Timer(MAX_RUN_TIME, terminal_manager._dead_man_swicth)
+	logger.debug("starting terminal command")
 	return terminal_manager._run_command_wrapper()
