3aa3699e7ebec087e024836ff9f33ff2357ab481
Nicole Epp <nicole.epp@invenia.ca>
2016-10-25 19:58:08 -0500

    debugfslkdflsdjfl

--- bot/slack_clients.py
+++ bot/slack_clients.py
@@ -71,15 +71,15 @@ class SlackClients(object):
         )
 
         # Make sure the message gets sent to zac-testing at least
-        if 'error' in str(response):
+        if 'error' in response:
             response = str(response) + "\nOriginal message:\n" + msg_text
             self.send_message(response, TESTING_CHANNEL)
-        elif 'bot_id' in str(response) and self.bot_id is None:
-            self.send_message(str(response), TESTING_CHANNEL)
-            self.bot_id = response['bot_id']
+        elif self.bot_id is None and 'message' in response:
+            message = response['message']
+            if 'bot_id' in message:
+                self.bot_id = message['bot_id']
             msg_text = 'Initialized bot_id: ' + self.bot_id
             self.send_message(msg_text, TESTING_CHANNEL)
-        self.send_message(str(response), TESTING_CHANNEL)
         return response
 
     def send_message(self, msg_text, channel):
