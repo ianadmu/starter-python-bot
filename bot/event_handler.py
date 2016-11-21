1125610caefd04c2aba4f03e9b0fb46a97b9b067
Nicole Epp <nicole.epp@invenia.ca>
2016-11-21 15:00:03 -0600

    fix zac error spamming

--- bot/event_handler.py
+++ bot/event_handler.py
@@ -137,11 +137,16 @@ class RtmEventHandler(object):
 
             # Command line
             # logging.info("lower_txt: "+str(lower_txt.split()))
-            token = lower_txt.split()[0]
-            if token == '#>' or token == u'#&gt;':
-                # logging.info("entering terminal command mode")
-                self.msg_writer.write_terminal_command(lower_txt, channel_id)
-                return 0  # Do not continue execution
+            try:
+                token = lower_txt.split()[0]
+                if token == '#>' or token == u'#&gt;':
+                    # logging.info("entering terminal command mode")
+                    self.msg_writer.write_terminal_command(
+                        lower_txt, channel_id
+                    )
+                    return 0  # Do not continue execution
+            except:
+                pass
 
             # Return channel and user information
             if lower_txt == 'channelinfo':
