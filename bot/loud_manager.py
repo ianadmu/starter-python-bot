b4d2017dbf97271e00722fbdb30bf8bcfd326fee
malcolm-mergulhao <malcolm.mergulhao@norimaconsulting.com>
2016-12-11 17:15:27 -0600

    turned off persistance

--- bot/loud_manager.py
+++ bot/loud_manager.py
@@ -5,7 +5,7 @@ from persistance_manager import PersistanceManager
 class LoudManager(object):
     def __init__(self):
         self.loud_file = open(os.path.join('./resources', 'louds.txt'), 'a+')
-        self.persistance_manager  = PersistanceManager("/louds.txt", seed_file="../resources/seed_louds.txt")
+        # self.persistance_manager  = PersistanceManager("/louds.txt", seed_file="../resources/seed_louds.txt")
 
     def write_loud_to_file(self, loudMessage):
         loudMessage = loudMessage.encode("utf8")
