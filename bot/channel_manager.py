6058ca66ece79f41ce1e6c0945a681fdb12b8b39
Garrett Suss <garrettsuss@gmail.com>
2017-01-07 11:27:07 -0600

    added group and im support for channel_manager

--- bot/channel_manager.py
+++ bot/channel_manager.py
@@ -4,6 +4,8 @@ class ChannelManager:
     def __init__(self, slack_clients):
         self.clients = slack_clients
         channels = self.clients.get_channels()
+        groups = self.clients.get_groups()
+        ims = self.clients.get_ims()
         self.channel_names = {}
         self.channel_ids = {}
         if channels['ok']:
@@ -11,6 +13,16 @@ class ChannelManager:
                 if channel['name'] not in ChannelManager.banned_channels:
                     self.channel_names[channel['id']] = channel['name']
                     self.channel_ids[channel['name']] = channel['id']
+
+        if groups['ok']:
+            for group in groups['groups']:
+                self.channel_names[group['id']] = group['name']
+                self.channel_names[group['name']] = group['id']
+                
+        if ims['ok']:
+            for im in ims['ims']:
+                self.channel_names[im['id']] = im['name']
+                self.channel_names[im['name']] = im['id']
 
     def get_channel_id(self, identifier):
         identifier = identifier.replace('#', '')
